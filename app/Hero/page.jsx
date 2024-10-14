7354b4d8e2e4bb26a42df6f84161396e74d09f1e
MateusSansão <mateuss12354678@gmail.com>
2024-10-13 22:10:14 -0300

    push

--- app/Hero/page.jsx
+++ app/Hero/page.jsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import "./page.css"
 
+
 export default function Hero() {
+
     return(
         <div className="hero-container">
             <Header/>
@@ -14,23 +16,23 @@ export default function Hero() {
                     <motion.h1 
                         initial={{ opacity: 0, x: -100 }} 
                         animate={{ opacity: 1, x: 0 }}     
-                        transition={{ duration: 1 }}  
-                        className="hero-title">
+                        transition={{ duration: 1, delay: 1}}  
+                        className="hero-title1">
                         Front end
                     </motion.h1>
 
                     <motion.h1 
                         initial={{ opacity: 0, x: -100 }} 
                         animate={{ opacity: 1, x: 0 }}     
-                        transition={{ duration: 1 }} 
-                        className="hero-title">
+                        transition={{ duration: 1, delay: 1.5 }} 
+                        className="hero-title2">
                         Developer
                     </motion.h1>
 
                     <motion.p 
                         initial={{ opacity: 0, x: -100 }} 
                         animate={{ opacity: 1, x: 0 }}     
-                        transition={{ duration: 1 }} 
+                        transition={{ duration: 1, delay: 2 }} 
                         className="hero-name">
                         Mateus Sansão Pereira
                     </motion.p>
